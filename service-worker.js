e90a266503c72775cd11a7faa2631ed4c5b2476f
AliceCheon <scaniscrini@gmail.com>
2026-07-09 17:19:44 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME = "atlas-app-v29";
+﻿const CACHE_NAME = "atlas-app-v30";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -41,6 +41,7 @@ self.addEventListener("fetch", (event) => {
     )
   );
 });
+
 
 
 
